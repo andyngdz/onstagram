3b3d830f6b95fc6c2176f34576947b8ad2f957f5
Andy Ng <itc.anhduy@gmail.com>
2021-03-19 16:42:35 +0700

    Add Header

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Home,
+  WhatsApp,
+  FavoriteBorder,
+  ExploreOutlined,
+} from "@material-ui/icons";
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  box: {
+    display: "grid",
+    gridAutoFlow: "column",
+    gridGap: theme.spacing(2),
+  },
+}));
+
+const Header = () => {
+  const style = useStyle();
+
+  return (
+    <AppBar color="transparent" variant="outlined" position="sticky">
+      <Toolbar className={style.root}>
+        <Box>
+          <Typography variant="h5">Instagram</Typography>
+        </Box>
+        <Box className={style.box}>
+          <Home />
+          <WhatsApp />
+          <FavoriteBorder />
+          <ExploreOutlined />
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
